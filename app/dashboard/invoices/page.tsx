fea9cee9d28c5a0f967f38aa80eef529db8abb9a
uronico <uronico3@gmail.com>
2025-11-24 01:48:15 +0800

    fixed error

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -12,7 +12,7 @@ import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Invoices | Acme Dashboard',
 };
- 
+
 export default async function Page(props: {
   searchParams?: Promise<{
     query?: string;
@@ -24,14 +24,12 @@ export default async function Page(props: {
   const currentPage = Number(searchParams?.page) || 1;
   const totalPages = await fetchInvoicesPages(query);
 
-
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        {/* @ts-expect-error Server Component */}
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
