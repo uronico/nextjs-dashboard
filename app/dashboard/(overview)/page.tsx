e4988601c8224e9b5d8a3a2f23feb98fce27b7b0
uronico <uronico3@gmail.com>
2025-11-24 02:11:00 +0800

    fixed error

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,14 +4,21 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton, } from '@/app/ui/skeletons';
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+  CardsSkeleton,
+} from '@/app/ui/skeletons';
 import CardWrapper from '@/app/ui/dashboard/cards';
 
 export default async function Page() {
-  
-  
-  const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
-  
+  const {
+    totalPaidInvoices,
+    totalPendingInvoices,
+    numberOfInvoices,
+    numberOfCustomers,
+  } = await fetchCardData();
+
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -19,11 +26,11 @@ export default async function Page() {
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardsSkeleton />}>
-          <CardWrapper 
-            totalPaidInvoices={totalPaidInvoices}
-            totalPendingInvoices={totalPendingInvoices}
-            numberOfInvoices={numberOfInvoices}
-            numberOfCustomers={numberOfCustomers}
+          <CardWrapper
+            totalPaidInvoices={Number(totalPaidInvoices)}
+            totalPendingInvoices={Number(totalPendingInvoices)}
+            numberOfInvoices={Number(numberOfInvoices)}
+            numberOfCustomers={Number(numberOfCustomers)}
           />
         </Suspense>
       </div>
